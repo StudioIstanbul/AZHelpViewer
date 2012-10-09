3663de61d0c35094a2e361f282cde57829dd4c7f
Andreas Zöllner <andi@go-for-fun.de>
2012-10-09 21:56:28 +0300

    initial commit

---
+++ AZHelpViewer.h
@@ -0,0 +1,13 @@
+//
+//  AZHelpViewer.h
+//  exchangeExport
+//
+//  Created by AVID Editor on 10/9/12.
+//  Copyright (c) 2012 Studio Istanbul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AZHelpViewer : NSObject
+
+@end
---
+++ AZHelpViewer.m
@@ -0,0 +1,13 @@
+//
+//  AZHelpViewer.m
+//  exchangeExport
+//
+//  Created by AVID Editor on 10/9/12.
+//  Copyright (c) 2012 Studio Istanbul. All rights reserved.
+//
+
+#import "AZHelpViewer.h"
+
+@implementation AZHelpViewer
+
+@end
